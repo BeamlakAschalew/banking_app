c9ba60b23c4ad101444aad2c942213487c8baa05
Beamlak Aschalew <birrletej12@gmail.com>
2023-05-30 13:09:48 +0300

    variable declaration, basic things with switch-case

--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
 int main() {
 
+    const int maxAccounts = 50;
 
+    string names[maxAccounts];
+    double balances[maxAccounts];
+    string passwords[maxAccounts];
+    int accountNumbers[maxAccounts];
+    string secretCodes[maxAccounts];
+
+    int userChoice;
+
+
+
+    while (true) {
+        cout << "1. Create an account";
+        cin >> userChoice;
+
+
+        switch (userChoice) {
+        case 1:
+            cout << "Enter Name: " << endl;
+            getline(cin.ignore(), names[0]);
+
+            
+            break;
+        
+        default:
+            break;
+        }
+
+        cout << names[0];
+        break;
+    }
 
     return 0;
 }
