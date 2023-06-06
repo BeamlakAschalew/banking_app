44675aa9922e91d37643a30008dc44eb2a602c6c
Beamlak Aschalew <birrletej12@gmail.com>
2023-06-06 10:14:34 +0300

    Added withdraw and transfer functionality.

--- main.cpp
+++ main.cpp
@@ -29,19 +29,20 @@ int main() {
                 "|______/  /__/     \\__\\ |__| \\__| |__|\\__\\ |__| |__| \\__|  \\______|    /__/     \\__\\ | _|      | _|      \n";
         cout << "1. Create an account: \n";
         cout << "2. Deposit money into your account: \n";
+        cout << "3. Withdraw\n";
         cin >> userChoice;
 
 
         switch (userChoice) {
             case 1: {
                 system("cls");
-                cout << "Great. You wanted to create an account! Follow the following procedures." << endl;
+                cout << "Great. You wanted to create an account! Follow the following procedures.";
 
-                cout << "Enter your name: " << endl;
+                cout << "\nEnter your name:";
                 getline(cin.ignore(), names[currentAccountIndex]);
 
                 passRetry:
-                cout << "Enter the password for your account: " << endl;
+                cout << "\nEnter the password for your account: " << endl;
                 getline(cin, tempPass);
 
                 if (tempPass.length() < 7) {
@@ -187,6 +188,210 @@ int main() {
 
 
             }
+
+            break;
+
+            case 3: {
+                system("cls");
+                cout << "Withdraw from your account: \n";
+
+                bool accountFound = false;
+                int accountIndex;
+
+                while (accountFound == 0) {
+                    cout << "Enter your account number: \n";
+
+                    int accountNumber;
+
+                    cin >> accountNumber;
+
+                    for (int i = 0; i < maxAccounts; i++) {
+                        if (accountNumbers[i] == accountNumber) {
+                            accountFound = true;
+                            accountIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (accountFound == 0) {
+                        cout << "The account number you entered is not found, try again.\n";
+                    }
+                }
+
+                string accountPassword; bool passwordValidated = false; int passwordTries = 4;
+
+
+                cout << "Welcome back, " << names[accountIndex] << "!\n";
+                while (!passwordValidated && passwordTries > 0) {
+                    cout << "Please enter the password of your account: \n";
+                    getline(cin.ignore(), accountPassword);
+
+                    if (accountPassword == passwords[accountIndex]) {
+                        passwordValidated = true;
+
+                    } else {
+                        cout << "The password you entered is incorrect!\n";
+                        --passwordTries;
+                        if (passwordTries >= 1)
+                            cout << "You have " << passwordTries << " trials left, try again.\n";
+                        else
+                            break;
+                    }
+                }
+
+                char choice;
+
+                if (passwordValidated) {
+                    withdrawAmountIterator: double moneyAmount;
+                    cout << "Enter the amount of money you want to withdraw: ";
+                    cin >> moneyAmount;
+
+                    if ((balances[accountIndex] - moneyAmount) <= 25) {
+                        cout << "Your balance is insufficient!" << "Your balance is " << balances[accountIndex] << "birr\n";
+                        goto withdrawAmountIterator;
+                    } else {
+                        balances[accountIndex] -= moneyAmount;
+                        cout << "You have successfully withdrawn " << moneyAmount << "birr from your account.\nYour current balance: " << balances[accountIndex] << "birr\nClick 'Y' if you want to go to main menu else click 'N' if you want to quit.\n";
+                        cin >> choice;
+
+                        if (choice == 'Y' || choice == 'y') {
+                            goto menuIterator;
+                        } else {
+                            break;
+                        }
+                    }
+
+
+                } else {
+
+                    cout << "\nYou have entered incorrect password too many times so we can't grant deposition action now. Click 'Y' if you want to go to main menu else click 'N' if you want to quit.\n";
+                    cin >> choice;
+
+                    if (choice == 'Y' || choice == 'y') {
+                        goto menuIterator;
+                    } else {
+                        break;
+                    }
+                }
+
+
+            }
+
+            break;
+
+            case 4: {
+                system("cls");
+                cout << "Transfer: \n";
+
+                bool accountFound = false;
+                int accountIndex;
+
+                while (accountFound == 0) {
+                    cout << "Enter your account number: \n";
+
+                    int accountNumber;
+
+                    cin >> accountNumber;
+
+                    for (int i = 0; i < maxAccounts; i++) {
+                        if (accountNumbers[i] == accountNumber) {
+                            accountFound = true;
+                            accountIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (accountFound == 0) {
+                        cout << "The account number you entered is not found, try again.\n";
+                    }
+                }
+
+                string accountPassword; bool passwordValidated = false; int passwordTries = 4;
+
+
+                cout << "Welcome back, " << names[accountIndex] << "!\n";
+                while (!passwordValidated && passwordTries > 0) {
+                    cout << "Please enter the password of your account: \n";
+                    getline(cin.ignore(), accountPassword);
+
+                    if (accountPassword == passwords[accountIndex]) {
+                        passwordValidated = true;
+
+                    } else {
+                        cout << "The password you entered is incorrect!\n";
+                        --passwordTries;
+                        if (passwordTries >= 1)
+                            cout << "You have " << passwordTries << " trials left, try again.\n";
+                        else
+                            break;
+                    }
+                }
+
+                char choice;
+
+                if (passwordValidated) {
+                    transferAmountIterator: double moneyAmount;
+                    bool recieverAccountFound = false;
+                    int recieverAccountIndex;
+
+                    while (accountFound == 0) {
+                        cout << "Enter the receiver's account number: \n";
+
+                        int receiverAccountNumber;
+
+                        cin >> receiverAccountNumber;
+
+                        for (int i = 0; i < maxAccounts; i++) {
+                            if (accountNumbers[i] == receiverAccountNumber) {
+                                recieverAccountFound = true;
+                                recieverAccountIndex = i;
+                                break;
+                            }
+                        }
+
+                        if (accountFound == 0) {
+                            cout << "The account number you entered is not found, try again.\n";
+                        }
+                    }
+
+
+                    cout << "Enter the amount of money you want to transfer: ";
+                    cin >> moneyAmount;
+
+                    if ((balances[accountIndex] - moneyAmount) <= 25) {
+                        cout << "Your balance is insufficient!" << "Your balance is " << balances[accountIndex] << "birr\n";
+                        goto transferAmountIterator;
+                    } else {
+                        balances[accountIndex] -= moneyAmount;
+                        cout << "You have successfully transferred " << moneyAmount << "birr from your account. To " << names[recieverAccountIndex] << "\nYour current balance: " << balances[accountIndex] << "birr\nClick 'Y' if you want to go to main menu else click 'N' if you want to quit.\n";
+                        cin >> choice;
+
+                        if (choice == 'Y' || choice == 'y') {
+                            goto menuIterator;
+                        } else {
+                            break;
+                        }
+                    }
+
+
+                } else {
+
+                    cout << "\nYou have entered incorrect password too many times so we can't grant deposition action now. Click 'Y' if you want to go to main menu else click 'N' if you want to quit.\n";
+                    cin >> choice;
+
+                    if (choice == 'Y' || choice == 'y') {
+                        goto menuIterator;
+                    } else {
+                        break;
+                    }
+                }
+
+
+            }
+
+
+
+
                 
 
             default:
